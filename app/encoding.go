782c3e9b12be7866ed216c7462e823973b5dfe8b
Ashis <pradhanashish95@gmail.com>
2025-03-19 17:40:54 +0530

    spawn chain with evm module

--- app/encoding.go
+++ app/encoding.go
@@ -1,36 +1,64 @@
 package app
 
 import (
-	// "github.com/cosmos/cosmos-sdk/codec"
-	// "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/std"
-	// "github.com/cosmos/cosmos-sdk/x/auth/tx"
-	evmenc "github.com/zeta-chain/ethermint/encoding"
-	ethermint "github.com/zeta-chain/ethermint/types"
-	// "pushchain/app/params"
+	"testing"
+
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/gogoproto/proto"
+
+	"cosmossdk.io/log"
+	"cosmossdk.io/x/tx/signing"
+
+	"github.com/cosmos/cosmos-sdk/codec/address"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	"github.com/rollchains/pchain/app/params"
 )
 
-// makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
-// func makeEncodingConfig() params.EncodingConfig {
-// 	amino := codec.NewLegacyAmino()
-// 	interfaceRegistry := types.NewInterfaceRegistry()
-// 	marshaler := codec.NewProtoCodec(interfaceRegistry)
-// 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+// MakeEncodingConfig creates a new EncodingConfig with all modules registered. For testing only
+func MakeEncodingConfig(t testing.TB) params.EncodingConfig {
+	t.Helper()
+	// we "pre"-instantiate the application for getting the injected/configured encoding configuration
+	// note, this is not necessary when using app wiring, as depinject can be directly used (see root_v2.go)
+	tempApp := NewChainApp(
+		log.NewNopLogger(),
+		dbm.NewMemDB(),
+		nil,
+		true,
+		simtestutil.NewAppOptionsWithFlagHome(t.TempDir()),
+		[]wasmkeeper.Option{},
+		EVMAppOptions,
+	)
+	return makeEncodingConfig(tempApp)
+}
 
-// 	return params.EncodingConfig{
-// 		InterfaceRegistry: interfaceRegistry,
-// 		Marshaler:         marshaler,
-// 		TxConfig:          txCfg,
-// 		Amino:             amino,
-// 	}
-// }
-
-// MakeEncodingConfig creates an EncodingConfig for testing
-func MakeEncodingConfig() ethermint.EncodingConfig {
-	encodingConfig := evmenc.MakeConfig(ModuleBasics)
-	// std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-	// ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+func makeEncodingConfig(tempApp *ChainApp) params.EncodingConfig {
+	encodingConfig := params.EncodingConfig{
+		InterfaceRegistry: tempApp.InterfaceRegistry(),
+		Codec:             tempApp.AppCodec(),
+		TxConfig:          tempApp.TxConfig(),
+		Amino:             tempApp.LegacyAmino(),
+	}
 	return encodingConfig
+}
+
+func GetInterfaceRegistry() types.InterfaceRegistry {
+	interfaceRegistry, err := types.NewInterfaceRegistryWithOptions(types.InterfaceRegistryOptions{
+		ProtoFiles: proto.HybridResolver,
+		SigningOptions: signing.Options{
+			AddressCodec: address.Bech32Codec{
+				Bech32Prefix: sdk.GetConfig().GetBech32AccountAddrPrefix(),
+			},
+			ValidatorAddressCodec: address.Bech32Codec{
+				Bech32Prefix: sdk.GetConfig().GetBech32ValidatorAddrPrefix(),
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	return interfaceRegistry
 }

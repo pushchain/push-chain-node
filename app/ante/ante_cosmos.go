fcf5d7d08468e97cf83cb437244aae8d66322d7d
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-05-13 16:25:46 +0530

    feat: made crosschain module messages gasless

--- app/ante/ante_cosmos.go
+++ app/ante/ante_cosmos.go
@@ -11,6 +11,7 @@ import (
 	circuitante "cosmossdk.io/x/circuit/ante"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	ibcante "github.com/cosmos/ibc-go/v8/modules/core/ante"
+	cosmosante "github.com/rollchains/pchain/app/cosmos"
 )
 
 // newCosmosAnteHandler creates the default ante handler for Cosmos transactions
@@ -32,9 +33,9 @@ func NewCosmosAnteHandler(options HandlerOptions) sdk.AnteHandler {
 		ante.NewValidateBasicDecorator(),
 		ante.NewTxTimeoutHeightDecorator(),
 		ante.NewValidateMemoDecorator(options.AccountKeeper),
-		evmoscosmosante.NewMinGasPriceDecorator(options.FeeMarketKeeper, options.EvmKeeper),
+		cosmosante.NewMinGasPriceDecorator(options.FeeMarketKeeper, options.EvmKeeper),
 		ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
-		ante.NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.TxFeeChecker),
+		NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.TxFeeChecker),
 		// SetPubKeyDecorator must be called before all signature verification decorators
 		ante.NewSetPubKeyDecorator(options.AccountKeeper),
 		ante.NewValidateSigCountDecorator(options.AccountKeeper),

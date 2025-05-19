0003e84a526827dadc31efe8fac613c75f5f31cc
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-05-19 13:50:22 +0530

    refactor: added param validation

--- x/crosschain/types/genesis_test.go
+++ x/crosschain/types/genesis_test.go
@@ -19,6 +19,11 @@ func TestGenesisState_Validate(t *testing.T) {
 			genState: types.DefaultGenesis(),
 			valid:    true,
 		},
+		{
+			desc:     "valid genesis state",
+			genState: &types.GenesisState{},
+			valid:    true,
+		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.desc, func(t *testing.T) {

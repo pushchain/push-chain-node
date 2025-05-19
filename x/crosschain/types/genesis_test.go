b2238782a01a318f2458f761697170984cf59458
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-05-19 13:42:53 +0530

    refactor: added config in types_test package

--- x/crosschain/types/genesis_test.go
+++ x/crosschain/types/genesis_test.go
@@ -19,11 +19,6 @@ func TestGenesisState_Validate(t *testing.T) {
 			genState: types.DefaultGenesis(),
 			valid:    true,
 		},
-		{
-			desc:     "valid genesis state",
-			genState: &types.GenesisState{},
-			valid:    true,
-		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.desc, func(t *testing.T) {

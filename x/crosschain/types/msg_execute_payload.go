22c97b705484c6fb8ec0896aaf6cac75ab393867
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-05-19 13:08:17 +0530

    refactor: added parameter validations in crosschain module

--- x/crosschain/types/msg_execute_payload.go
+++ x/crosschain/types/msg_execute_payload.go
@@ -1,8 +1,13 @@
 package types
 
 import (
+	"encoding/hex"
+	"strings"
+
 	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/rollchains/pchain/util"
 )
 
 var (
@@ -43,10 +48,41 @@ func (msg *MsgExecutePayload) GetSigners() []sdk.AccAddress {
 
 // ValidateBasic does a sanity check on the provided data.
 func (msg *MsgExecutePayload) Validate() error {
+	// Validate signer
 	if _, err := sdk.AccAddressFromBech32(msg.Signer); err != nil {
-		return errors.Wrap(err, "invalid authority address")
+		return errors.Wrap(err, "invalid signer address")
 	}
 
-	// TODO: sanity check
+	// Validate caipString
+	if len(msg.CaipString) == 0 {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "caipString cannot be empty")
+	}
+
+	// Validate crosschain payload
+	if msg.CrosschainPayload == nil {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "crosschain payload cannot be nil")
+	}
+
+	// Validate target address
+	isValidTarget := util.IsValidAddress(msg.CrosschainPayload.Target, util.HEX)
+	if !isValidTarget {
+		return errors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid target address format: %s", msg.CrosschainPayload.Target)
+	}
+
+	// Validate data (hex string)
+	if len(msg.CrosschainPayload.Data) > 0 {
+		if _, err := hex.DecodeString(strings.TrimPrefix(msg.CrosschainPayload.Data, "0x")); err != nil {
+			return errors.Wrap(sdkerrors.ErrInvalidRequest, "invalid hex data")
+		}
+	}
+
+	// Validate signature
+	if len(msg.Signature) == 0 {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "signature cannot be empty")
+	}
+	if _, err := hex.DecodeString(strings.TrimPrefix(msg.Signature, "0x")); err != nil {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature hex")
+	}
+
 	return nil
 }
--- x/crosschain/types/msg_mint_push.go
+++ x/crosschain/types/msg_mint_push.go
@@ -3,6 +3,8 @@ package types
 import (
 	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 var (
@@ -26,7 +28,7 @@ func NewMsgMintPush(
 func (msg MsgMintPush) Route() string { return ModuleName }
 
 // Type returns the the action
-func (msg MsgMintPush) Type() string { return "deploy_nmsc" }
+func (msg MsgMintPush) Type() string { return "mint_push" }
 
 // GetSignBytes implements the LegacyMsg interface.
 func (msg MsgMintPush) GetSignBytes() []byte {
@@ -42,9 +44,18 @@ func (msg *MsgMintPush) GetSigners() []sdk.AccAddress {
 // ValidateBasic does a sanity check on the provided data.
 func (msg *MsgMintPush) Validate() error {
 	if _, err := sdk.AccAddressFromBech32(msg.Signer); err != nil {
-		return errors.Wrap(err, "invalid authority address")
+		return errors.Wrap(err, "invalid signer address")
 	}
 
-	// TODO: sanity check
+	// Validate caipString
+	if len(msg.CaipString) == 0 {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "caipString cannot be empty")
+	}
+
+	// Validate txHash
+	if len(msg.TxHash) == 0 {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "txHash cannot be empty")
+	}
+
 	return nil
 }

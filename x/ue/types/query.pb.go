11b00a40d9de797588dd71ac5788da37c6efe93f
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-06-03 13:20:52 +0530

    refactor: added protobuf generated files of chain config msg

--- x/ue/types/query.pb.go
+++ x/ue/types/query.pb.go
@@ -194,37 +194,135 @@ func (m *QueryAdminParamsResponse) GetAdminParams() *AdminParams {
 	return nil
 }
 
+// QueryChainConfigRequest is the request type for querying a ChainConfig by chain_id.
+type QueryChainConfigRequest struct {
+	ChainId string `protobuf:"bytes,1,opt,name=chain_id,json=chainId,proto3" json:"chain_id,omitempty"`
+}
+
+func (m *QueryChainConfigRequest) Reset()         { *m = QueryChainConfigRequest{} }
+func (m *QueryChainConfigRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryChainConfigRequest) ProtoMessage()    {}
+func (*QueryChainConfigRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4f584bc3b15bf59c, []int{4}
+}
+func (m *QueryChainConfigRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryChainConfigRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryChainConfigRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryChainConfigRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryChainConfigRequest.Merge(m, src)
+}
+func (m *QueryChainConfigRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryChainConfigRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryChainConfigRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryChainConfigRequest proto.InternalMessageInfo
+
+func (m *QueryChainConfigRequest) GetChainId() string {
+	if m != nil {
+		return m.ChainId
+	}
+	return ""
+}
+
+// QueryChainConfigResponse is the response type containing the ChainConfig.
+type QueryChainConfigResponse struct {
+	Config *ChainConfig `protobuf:"bytes,1,opt,name=config,proto3" json:"config,omitempty"`
+}
+
+func (m *QueryChainConfigResponse) Reset()         { *m = QueryChainConfigResponse{} }
+func (m *QueryChainConfigResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryChainConfigResponse) ProtoMessage()    {}
+func (*QueryChainConfigResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4f584bc3b15bf59c, []int{5}
+}
+func (m *QueryChainConfigResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryChainConfigResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryChainConfigResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryChainConfigResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryChainConfigResponse.Merge(m, src)
+}
+func (m *QueryChainConfigResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryChainConfigResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryChainConfigResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryChainConfigResponse proto.InternalMessageInfo
+
+func (m *QueryChainConfigResponse) GetConfig() *ChainConfig {
+	if m != nil {
+		return m.Config
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*QueryParamsRequest)(nil), "ue.v1.QueryParamsRequest")
 	proto.RegisterType((*QueryParamsResponse)(nil), "ue.v1.QueryParamsResponse")
 	proto.RegisterType((*QueryAdminParamsRequest)(nil), "ue.v1.QueryAdminParamsRequest")
 	proto.RegisterType((*QueryAdminParamsResponse)(nil), "ue.v1.QueryAdminParamsResponse")
+	proto.RegisterType((*QueryChainConfigRequest)(nil), "ue.v1.QueryChainConfigRequest")
+	proto.RegisterType((*QueryChainConfigResponse)(nil), "ue.v1.QueryChainConfigResponse")
 }
 
 func init() { proto.RegisterFile("ue/v1/query.proto", fileDescriptor_4f584bc3b15bf59c) }
 
 var fileDescriptor_4f584bc3b15bf59c = []byte{
-	// 315 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2c, 0x4d, 0xd5, 0x2f,
-	0x33, 0xd4, 0x2f, 0x2c, 0x4d, 0x2d, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2d,
-	0x4d, 0xd5, 0x2b, 0x33, 0x94, 0x92, 0x49, 0xcf, 0xcf, 0x4f, 0xcf, 0x49, 0xd5, 0x4f, 0x2c, 0xc8,
-	0xd4, 0x4f, 0xcc, 0xcb, 0xcb, 0x2f, 0x49, 0x2c, 0xc9, 0xcc, 0xcf, 0x2b, 0x86, 0x28, 0x92, 0x12,
-	0x82, 0xe8, 0x2b, 0x48, 0x2c, 0x4a, 0xcc, 0x85, 0x8a, 0x29, 0x89, 0x70, 0x09, 0x05, 0x82, 0xcc,
-	0x09, 0x00, 0x0b, 0x06, 0xa5, 0x16, 0x96, 0xa6, 0x16, 0x97, 0x28, 0xd9, 0x70, 0x09, 0xa3, 0x88,
-	0x16, 0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x0a, 0xa9, 0x72, 0xb1, 0x41, 0x34, 0x4b, 0x30, 0x2a, 0x30,
-	0x6a, 0x70, 0x1b, 0xf1, 0xea, 0x81, 0xad, 0xd5, 0x83, 0x2a, 0x83, 0x4a, 0x2a, 0x49, 0x72, 0x89,
-	0x83, 0x75, 0x3b, 0xa6, 0xe4, 0x66, 0xe6, 0xa1, 0x1a, 0x1c, 0xc8, 0x25, 0x81, 0x29, 0x05, 0x35,
-	0xdd, 0x94, 0x8b, 0x27, 0x11, 0x24, 0x1c, 0x8f, 0x62, 0x87, 0x10, 0xd4, 0x0e, 0x64, 0x1d, 0xdc,
-	0x89, 0x08, 0x8e, 0xd1, 0x19, 0x46, 0x2e, 0x56, 0xb0, 0x99, 0x42, 0x61, 0x5c, 0x6c, 0x10, 0x31,
-	0x21, 0x49, 0xa8, 0x26, 0x4c, 0xaf, 0x49, 0x49, 0x61, 0x93, 0x82, 0xb8, 0x40, 0x49, 0xb4, 0xe9,
-	0xf2, 0x93, 0xc9, 0x4c, 0xfc, 0x42, 0xbc, 0xfa, 0xc8, 0x21, 0x25, 0x94, 0xcd, 0xc5, 0x8d, 0x64,
-	0xbb, 0x90, 0x1c, 0xb2, 0x09, 0x98, 0x7e, 0x94, 0x92, 0xc7, 0x29, 0x0f, 0xb5, 0x46, 0x1a, 0x6c,
-	0x8d, 0xa8, 0x90, 0x30, 0xd4, 0x1a, 0x64, 0x5f, 0x3b, 0x39, 0x9e, 0x78, 0x24, 0xc7, 0x78, 0xe1,
-	0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70,
-	0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x7a, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e, 0xae,
-	0x7e, 0x51, 0x7e, 0x4e, 0x4e, 0x72, 0x46, 0x62, 0x66, 0x5e, 0xb1, 0x7e, 0x01, 0x98, 0xd6, 0xaf,
-	0x00, 0x19, 0x56, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0x8e, 0x5a, 0x63, 0x40, 0x00, 0x00,
-	0x00, 0xff, 0xff, 0xe1, 0xa1, 0x53, 0x0d, 0x28, 0x02, 0x00, 0x00,
+	// 411 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0x4f, 0x4f, 0xe2, 0x40,
+	0x18, 0xc6, 0x29, 0x09, 0xdd, 0xdd, 0xe9, 0x92, 0xcd, 0x0e, 0x4b, 0x16, 0xaa, 0x19, 0x49, 0x13,
+	0xff, 0xc4, 0x43, 0x27, 0xa0, 0xde, 0xbc, 0x20, 0x89, 0x89, 0x37, 0xe1, 0xe0, 0xc1, 0x0b, 0x19,
+	0x60, 0x2c, 0x8d, 0xd0, 0x29, 0xed, 0x94, 0x48, 0x8c, 0x17, 0x3f, 0x81, 0x89, 0x5f, 0xc9, 0x83,
+	0x47, 0x12, 0x2f, 0x1e, 0x0d, 0xf8, 0x41, 0x4c, 0x67, 0x86, 0xd8, 0x5a, 0x3c, 0x35, 0xf3, 0xbe,
+	0xcf, 0xfb, 0xfc, 0xde, 0x79, 0xa6, 0xe0, 0x6f, 0x44, 0xf1, 0xb4, 0x8e, 0x27, 0x11, 0x0d, 0x66,
+	0xb6, 0x1f, 0x30, 0xce, 0x60, 0x21, 0xa2, 0xf6, 0xb4, 0x6e, 0x6e, 0x3a, 0x8c, 0x39, 0x23, 0x8a,
+	0x89, 0xef, 0x62, 0xe2, 0x79, 0x8c, 0x13, 0xee, 0x32, 0x2f, 0x94, 0x22, 0x13, 0xca, 0x39, 0x9f,
+	0x04, 0x64, 0xbc, 0xaa, 0x29, 0xaf, 0x90, 0x13, 0x4e, 0x65, 0xc9, 0xfa, 0x07, 0x60, 0x3b, 0xb6,
+	0x3e, 0x17, 0xba, 0x0e, 0x9d, 0x44, 0x34, 0xe4, 0xd6, 0x31, 0x28, 0xa5, 0xaa, 0xa1, 0xcf, 0xbc,
+	0x90, 0xc2, 0x6d, 0xa0, 0x4b, 0xbf, 0x8a, 0x56, 0xd3, 0xf6, 0x8c, 0x46, 0xd1, 0x16, 0x9b, 0xd8,
+	0x4a, 0xa6, 0x9a, 0x56, 0x15, 0xfc, 0x17, 0xd3, 0xcd, 0xc1, 0xd8, 0xf5, 0xd2, 0xc6, 0x6d, 0x50,
+	0xc9, 0xb6, 0x94, 0xfb, 0x11, 0xf8, 0x4d, 0xe2, 0x72, 0x37, 0xc5, 0x80, 0x8a, 0x91, 0x9c, 0x30,
+	0xc8, 0xe7, 0xc1, 0x3a, 0x54, 0xb4, 0xd6, 0x90, 0xb8, 0x5e, 0x8b, 0x79, 0x57, 0xae, 0xa3, 0x68,
+	0xb0, 0x0a, 0x7e, 0xf6, 0xe3, 0x6a, 0xd7, 0x1d, 0x08, 0xb7, 0x5f, 0x9d, 0x1f, 0xe2, 0x7c, 0x36,
+	0xb0, 0x4e, 0xd5, 0x22, 0xa9, 0x29, 0xb5, 0xc8, 0x3e, 0xd0, 0xfb, 0xa2, 0xf2, 0x65, 0x85, 0xa4,
+	0x56, 0x29, 0x1a, 0x4f, 0x79, 0x50, 0x10, 0x46, 0xf0, 0x02, 0xe8, 0x72, 0x23, 0x58, 0x55, 0xfa,
+	0x6c, 0xb0, 0xa6, 0xb9, 0xae, 0x25, 0xb1, 0x56, 0xf9, 0xfe, 0xe5, 0xfd, 0x31, 0xff, 0x07, 0x16,
+	0x71, 0xf2, 0xe9, 0xe0, 0x35, 0x30, 0x12, 0x77, 0x87, 0x28, 0xe9, 0x90, 0x4d, 0xd8, 0xdc, 0xfa,
+	0xb6, 0xaf, 0x30, 0x1b, 0x02, 0x53, 0x86, 0x25, 0x85, 0x49, 0x66, 0x0e, 0xa7, 0xc0, 0x48, 0xdc,
+	0x32, 0x0d, 0xcb, 0x06, 0x9c, 0x86, 0xad, 0x89, 0xd2, 0xda, 0x11, 0xb0, 0x1a, 0x44, 0x0a, 0x26,
+	0x9f, 0x43, 0x66, 0x87, 0x6f, 0x57, 0x8f, 0x73, 0x77, 0xd2, 0x7c, 0x5e, 0x20, 0x6d, 0xbe, 0x40,
+	0xda, 0xdb, 0x02, 0x69, 0x0f, 0x4b, 0x94, 0x9b, 0x2f, 0x51, 0xee, 0x75, 0x89, 0x72, 0x97, 0xbb,
+	0x8e, 0xcb, 0x87, 0x51, 0xcf, 0xee, 0xb3, 0x31, 0x0e, 0xd8, 0x68, 0x24, 0x66, 0x42, 0xec, 0x8b,
+	0x2f, 0xbe, 0x89, 0x7d, 0xf9, 0xcc, 0xa7, 0x61, 0x4f, 0x17, 0x3f, 0xf4, 0xc1, 0x47, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0x39, 0xe7, 0xc2, 0x2f, 0x31, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -243,6 +341,8 @@ type QueryClient interface {
 	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
 	// AdminParams queries all admin-controlled parameters of the module.
 	AdminParams(ctx context.Context, in *QueryAdminParamsRequest, opts ...grpc.CallOption) (*QueryAdminParamsResponse, error)
+	// ChainConfig queries a ChainConfig by chain_id.
+	ChainConfig(ctx context.Context, in *QueryChainConfigRequest, opts ...grpc.CallOption) (*QueryChainConfigResponse, error)
 }
 
 type queryClient struct {
@@ -271,12 +371,23 @@ func (c *queryClient) AdminParams(ctx context.Context, in *QueryAdminParamsReque
 	return out, nil
 }
 
+func (c *queryClient) ChainConfig(ctx context.Context, in *QueryChainConfigRequest, opts ...grpc.CallOption) (*QueryChainConfigResponse, error) {
+	out := new(QueryChainConfigResponse)
+	err := c.cc.Invoke(ctx, "/ue.v1.Query/ChainConfig", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// Params queries all parameters of the module.
 	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
 	// AdminParams queries all admin-controlled parameters of the module.
 	AdminParams(context.Context, *QueryAdminParamsRequest) (*QueryAdminParamsResponse, error)
+	// ChainConfig queries a ChainConfig by chain_id.
+	ChainConfig(context.Context, *QueryChainConfigRequest) (*QueryChainConfigResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -288,6 +399,9 @@ func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsReq
 }
 func (*UnimplementedQueryServer) AdminParams(ctx context.Context, req *QueryAdminParamsRequest) (*QueryAdminParamsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AdminParams not implemented")
+}
+func (*UnimplementedQueryServer) ChainConfig(ctx context.Context, req *QueryChainConfigRequest) (*QueryChainConfigResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ChainConfig not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -330,6 +444,24 @@ func _Query_AdminParams_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_ChainConfig_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryChainConfigRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).ChainConfig(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/ue.v1.Query/ChainConfig",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).ChainConfig(ctx, req.(*QueryChainConfigRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "ue.v1.Query",
 	HandlerType: (*QueryServer)(nil),
@@ -341,6 +473,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "AdminParams",
 			Handler:    _Query_AdminParams_Handler,
+		},
+		{
+			MethodName: "ChainConfig",
+			Handler:    _Query_ChainConfig_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -463,6 +599,71 @@ func (m *QueryAdminParamsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryChainConfigRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryChainConfigRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryChainConfigRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.ChainId) > 0 {
+		i -= len(m.ChainId)
+		copy(dAtA[i:], m.ChainId)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.ChainId)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryChainConfigResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryChainConfigResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryChainConfigResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Config != nil {
+		{
+			size, err := m.Config.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintQuery(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -513,6 +714,32 @@ func (m *QueryAdminParamsResponse) Size() (n int) {
 	_ = l
 	if m.AdminParams != nil {
 		l = m.AdminParams.Size()
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *QueryChainConfigRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.ChainId)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *QueryChainConfigResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.Config != nil {
+		l = m.Config.Size()
 		n += 1 + l + sovQuery(uint64(l))
 	}
 	return n
@@ -772,6 +999,174 @@ func (m *QueryAdminParamsResponse) Unmarshal(dAtA []byte) error {
 				m.AdminParams = &AdminParams{}
 			}
 			if err := m.AdminParams.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryChainConfigRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryChainConfigRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryChainConfigRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ChainId", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ChainId = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryChainConfigResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryChainConfigResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryChainConfigResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Config", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Config == nil {
+				m.Config = &ChainConfig{}
+			}
+			if err := m.Config.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex

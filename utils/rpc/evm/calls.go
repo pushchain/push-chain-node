e4914eb369096ae4649c97cc7587e1ec7fec8fe9
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-06-13 12:55:22 +0530

    refactor: modified evm, svm rpc functions

--- utils/rpc/evm/calls.go
+++ utils/rpc/evm/calls.go
@@ -8,25 +8,37 @@ import (
 )
 
 // EVMGetTransactionByHash fetches tx info
-func EVMGetTransactionByHash(ctx context.Context, rpcURL, txHash string) (*Transaction, error) {
+func EVMGetTransactionByHash(ctx context.Context, cfg rpc.RpcCallConfig, txHash string) (*Transaction, error) {
 	client := rpc.GetClient()
 
 	var result Transaction
-	fmt.Println(rpcURL)
-	err := client.Call(ctx, rpcURL, "eth_getTransactionByHash", []interface{}{txHash}, &result)
-	if err != nil {
+	var err error
+	if cfg.PrivateRPC != "" {
+		err = client.CallWithFallback(ctx, cfg.PrivateRPC, cfg.PublicRPC, "eth_getTransactionByHash", []interface{}{txHash}, &result)
+	} else {
 		fmt.Println("Error calling eth_getTransactionByHash:", err)
+		err = client.Call(ctx, cfg.PublicRPC, "eth_getTransactionByHash", []interface{}{txHash}, &result)
+	}
+
+	if err != nil {
 		return nil, fmt.Errorf("eth_getTransactionByHash failed: %w", err)
 	}
 	return &result, nil
 }
 
 // EVMGetTransactionReceipt fetches receipt + logs
-func EVMGetTransactionReceipt(ctx context.Context, rpcURL, txHash string) (*TransactionReceipt, error) {
+func EVMGetTransactionReceipt(ctx context.Context, cfg rpc.RpcCallConfig, txHash string) (*TransactionReceipt, error) {
 	client := rpc.GetClient()
 
 	var result TransactionReceipt
-	err := client.Call(ctx, rpcURL, "eth_getTransactionReceipt", []interface{}{txHash}, &result)
+	var err error
+	if cfg.PrivateRPC != "" {
+		err = client.CallWithFallback(ctx, cfg.PrivateRPC, cfg.PublicRPC, "eth_getTransactionReceipt", []interface{}{txHash}, &result)
+	} else {
+		fmt.Println("Error calling eth_getTransactionReceipt:", err)
+		err = client.Call(ctx, cfg.PublicRPC, "eth_getTransactionReceipt", []interface{}{txHash}, &result)
+	}
+
 	if err != nil {
 		fmt.Println("Error calling eth_getTransactionByHash:", err)
 		return nil, fmt.Errorf("eth_getTransactionReceipt failed: %w", err)
@@ -35,11 +47,18 @@ func EVMGetTransactionReceipt(ctx context.Context, rpcURL, txHash string) (*Tran
 }
 
 // EVMGetBlockByNumber fetches block details
-func EVMGetBlockByNumber(ctx context.Context, rpcURL, blockNumber string, fullTx bool) (*Block, error) {
+func EVMGetBlockByNumber(ctx context.Context, cfg rpc.RpcCallConfig, blockNumber string, fullTx bool) (*Block, error) {
 	client := rpc.GetClient()
 
 	var result Block
-	err := client.Call(ctx, rpcURL, "eth_getBlockByNumber", []interface{}{blockNumber, fullTx}, &result)
+	var err error
+	if cfg.PrivateRPC != "" {
+		err = client.CallWithFallback(ctx, cfg.PrivateRPC, cfg.PublicRPC, "eth_getBlockByNumber", []interface{}{blockNumber, fullTx}, &result)
+	} else {
+		fmt.Println("Error calling eth_getBlockByNumber:", err)
+		err = client.Call(ctx, cfg.PublicRPC, "eth_getBlockByNumber", []interface{}{blockNumber, fullTx}, &result)
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("eth_getBlockByNumber failed: %w", err)
 	}
--- utils/rpc/svm/calls.go
+++ utils/rpc/svm/calls.go
@@ -4,13 +4,22 @@ import (
 	"context"
 	"fmt"
 
-	baserpc "github.com/rollchains/pchain/utils/rpc"
+	rpc "github.com/rollchains/pchain/utils/rpc"
 )
 
 // GetTransaction fetches transaction details using getTransaction RPC method
-func SVMGetTransactionBySig(ctx context.Context, rpcURL, txHash string) (*Transaction, error) {
+func SVMGetTransactionBySig(ctx context.Context, cfg rpc.RpcCallConfig, txHash string) (*Transaction, error) {
+	client := rpc.GetClient()
+
 	var result Transaction
-	err := baserpc.GetClient().Call(ctx, rpcURL, "getTransaction", []interface{}{txHash}, &result)
+	var err error
+	if cfg.PrivateRPC != "" {
+		err = client.CallWithFallback(ctx, cfg.PrivateRPC, cfg.PublicRPC, "getTransaction", []interface{}{txHash}, &result)
+	} else {
+		fmt.Println("Error calling getTransaction:", err)
+		err = client.Call(ctx, cfg.PublicRPC, "getTransaction", []interface{}{txHash}, &result)
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("getTransaction failed: %w", err)
 	}
@@ -18,9 +27,18 @@ func SVMGetTransactionBySig(ctx context.Context, rpcURL, txHash string) (*Transa
 }
 
 // GetSlot fetches current slot using getSlot RPC method
-func SVMGetCurrentSlot(ctx context.Context, rpcURL string) (uint64, error) {
+func SVMGetCurrentSlot(ctx context.Context, cfg rpc.RpcCallConfig) (uint64, error) {
+	client := rpc.GetClient()
+
 	var result Slot
-	err := baserpc.GetClient().Call(ctx, rpcURL, "getSlot", nil, &result)
+	var err error
+	if cfg.PrivateRPC != "" {
+		err = client.CallWithFallback(ctx, cfg.PrivateRPC, cfg.PublicRPC, "getSlot", nil, &result)
+	} else {
+		fmt.Println("Error calling getSlot:", err)
+		err = client.Call(ctx, cfg.PublicRPC, "getSlot", nil, &result)
+	}
+
 	if err != nil {
 		return 0, fmt.Errorf("getSlot failed: %w", err)
 	}

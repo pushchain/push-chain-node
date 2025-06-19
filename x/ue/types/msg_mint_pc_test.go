26231dbec883d1bcc7102116636ee9ba15e5cf1f
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-06-19 18:51:15 +0530

    tests: added validations test cases for msg_mint_pc

---
+++ x/ue/types/msg_mint_pc_test.go
@@ -0,0 +1,86 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/rollchains/pchain/x/ue/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgMintPC_ValidateBasic(t *testing.T) {
+	validSigner := "push1fgaewhyd9fkwtqaj9c233letwcuey6dgly9gv9"
+	invalidSigner := "not_bech32"
+
+	validUA := &types.UniversalAccount{
+		Chain: "eip155:11155111",
+		Owner: "0x000000000000000000000000000000000000dead",
+	}
+
+	invalidUA := &types.UniversalAccount{
+		Chain: "solana", // invalid CAIP-2 format
+		Owner: "0xabc",
+	}
+
+	tests := []struct {
+		name      string
+		msg       *types.MsgMintPC
+		expectErr bool
+	}{
+		{
+			name: "valid message",
+			msg: &types.MsgMintPC{
+				Signer:           validSigner,
+				UniversalAccount: validUA,
+				TxHash:           "0x123abc",
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid signer address",
+			msg: &types.MsgMintPC{
+				Signer:           invalidSigner,
+				UniversalAccount: validUA,
+				TxHash:           "0x123abc",
+			},
+			expectErr: true,
+		},
+		{
+			name: "nil universal account",
+			msg: &types.MsgMintPC{
+				Signer:           validSigner,
+				UniversalAccount: nil,
+				TxHash:           "0x123abc",
+			},
+			expectErr: true,
+		},
+		{
+			name: "invalid universal account",
+			msg: &types.MsgMintPC{
+				Signer:           validSigner,
+				UniversalAccount: invalidUA,
+				TxHash:           "0x123abc",
+			},
+			expectErr: true,
+		},
+		{
+			name: "empty tx hash",
+			msg: &types.MsgMintPC{
+				Signer:           validSigner,
+				UniversalAccount: validUA,
+				TxHash:           "",
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.msg.ValidateBasic()
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}

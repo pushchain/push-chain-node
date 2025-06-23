a3ff10940922abb7875c7bcbbc1525a6220321c1
Zartaj0 <zartaj.afsar@gmail.com>
2025-06-23 19:00:46 +0530

    feat: Added enum for method type (removing method name)

--- x/ue/types/method_config.go
+++ x/ue/types/method_config.go
@@ -21,9 +21,9 @@ func (p MethodConfig) String() string {
 
 // Validate does the sanity check on the params.
 func (p MethodConfig) ValidateBasic() error {
-	// Name must not be empty
-	if strings.TrimSpace(p.Name) == "" {
-		return errors.Wrap(sdkerrors.ErrInvalidRequest, "method name cannot be empty")
+	// MethodType must be valid
+	if p.MethodType != GatewayMethodType_ADD_FUNDS {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "method type must be valid")
 	}
 
 	//Identifier must not be empty and must be valid hex

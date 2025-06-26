2c75de4bdcdba14f9b9ada295d47476bd02a449d
Mohammed S <shoaibmohammed92@gmail.com>
2025-06-27 02:46:56 +0530

    fix: update scripts and domains

--- deploy/test-push-chain/scripts/start.sh
+++ deploy/test-push-chain/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(cd ~ && nohup ~/app/pchaind start --pruning=nothing  --minimum-gas-prices=0npush --rpc.laddr="tcp://0.0.0.0:26657" --json-rpc.api=eth,txpool,personal,net,debug,web3,trace --json-rpc.address="0.0.0.0:8545" --json-rpc.ws-address="0.0.0.0:8546" --chain-id="push_42101-1" --trace --evm.tracer=json --home ~/.pchain >> ~/app/chain.log 2>&1 &)
+(cd ~ && nohup ~/app/pchaind start --pruning=nothing  --minimum-gas-prices=1000000000upc --rpc.laddr="tcp://0.0.0.0:26657" --json-rpc.api=eth,txpool,personal,net,debug,web3,trace --json-rpc.address="0.0.0.0:8545" --json-rpc.ws-address="0.0.0.0:8546" --chain-id="push_42101-1" --trace --evm.tracer=json --home ~/.pchain >> ~/app/chain.log 2>&1 &)

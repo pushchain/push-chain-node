90adadfbfffdf1161d2442a15a795446bd92483e
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-07-25 10:59:18 +0530

    refactor(registry): added msg_update_params in registry module

---
+++ x/registry/keeper/msg_update_params.go
@@ -0,0 +1,12 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/rollchains/pchain/x/registry/types"
+)
+
+// updateParams is for updating params collections of the module
+func (k Keeper) UpdateParams(ctx context.Context, params types.Params) error {
+	return k.Params.Set(ctx, params)
+}

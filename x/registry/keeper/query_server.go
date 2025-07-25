b7ebc2849e62b96fb67172bbbfb3c3280314e956
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-07-25 07:28:16 +0530

    refactor(registry): added generated code for registry module

---
+++ x/registry/keeper/query_server.go
@@ -0,0 +1,37 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/rollchains/pchain/x/registry/types"
+)
+
+var _ types.QueryServer = Querier{}
+
+type Querier struct {
+	Keeper
+}
+
+func NewQuerier(keeper Keeper) Querier {
+	return Querier{Keeper: keeper}
+}
+
+func (k Querier) Params(c context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+
+	p, err := k.Keeper.Params.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryParamsResponse{Params: &p}, nil
+}
+
+// ChainConfig implements types.QueryServer.
+func (k Querier) ChainConfig(goCtx context.Context, req *types.QueryChainConfigRequest) (*types.QueryChainConfigResponse, error) {
+	// ctx := sdk.UnwrapSDKContext(goCtx)
+	panic("ChainConfig is unimplemented")
+	return &types.QueryChainConfigResponse{}, nil
+}

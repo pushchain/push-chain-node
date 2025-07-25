0882326fd8c7ac879a6415cd9d5778b277ec2d2d
aman035 <guptaaman200115@gmail.com>
2025-07-25 21:10:29 +0530

    fix: logger

--- universalClient/logger/logger.go
+++ universalClient/logger/logger.go
@@ -1,25 +1,34 @@
 package logger
 
 import (
-	"log/slog"
+	"io"
 	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+
+	"github.com/rollchains/pchain/universalClient/config"
 )
 
-func New(logLevel string) *slog.Logger {
-	var level slog.Level
-	switch logLevel {
-	case "debug":
-		level = slog.LevelDebug
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		level = slog.LevelInfo
+// Init sets up the global zerolog logger based on config.
+// Supports console/json format, level filtering, and optional sampling.
+func Init(cfg config.Config) zerolog.Logger {
+	var writer io.Writer = os.Stdout
+	if cfg.LogFormat != "json" {
+		writer = zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: time.RFC3339,
+		}
 	}
 
-	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	})
-	return slog.New(handler)
+	logger := zerolog.New(writer).
+		Level(zerolog.Level(cfg.LogLevel)).
+		With().
+		Timestamp().
+		Logger()
+
+	if cfg.LogSampler {
+		logger = logger.Sample(&zerolog.BasicSampler{N: 5})
+	}
+	return logger
 }

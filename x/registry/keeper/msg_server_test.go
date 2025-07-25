10b56ead2def908b263c8fc7cc7c4e627f389c84
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-07-25 17:51:22 +0530

    tests(registry): added msg server tests

--- x/registry/keeper/msg_server_test.go
+++ x/registry/keeper/msg_server_test.go
@@ -54,3 +54,127 @@ func TestParams(t *testing.T) {
 		})
 	}
 }
+
+func TestMsgServer_AddChainConfig(t *testing.T) {
+	f := SetupTest(t)
+	validSigner := f.addrs[0]
+
+	chainConfigTest := types.ChainConfig{
+		Chain:          "eip:11155111",
+		VmType:         types.VmType_EVM, // replace with appropriate VM_TYPE enum value
+		PublicRpcUrl:   "https://mainnet.infura.io/v3/YOUR_PROJECT_ID",
+		GatewayAddress: "0x1234567890abcdef1234567890abcdef12345678",
+		BlockConfirmation: &types.BlockConfirmation{
+			FastInbound: 3,
+			SlowInbound: 6,
+		},
+		GatewayMethods: []*types.GatewayMethods{},
+		Enabled:        true,
+	}
+	t.Run("Failed to get params", func(t *testing.T) {
+		msg := &types.MsgAddChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+
+		_, err := f.msgServer.AddChainConfig(f.ctx, msg)
+		require.ErrorContains(t, err, "failed to get params")
+	})
+
+	t.Run("fail : Invalid authority", func(t *testing.T) {
+		msg := &types.MsgAddChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+		f.k.Params.Set(f.ctx, types.Params{})
+		_, err := f.msgServer.AddChainConfig(f.ctx, msg)
+		require.ErrorContains(t, err, "invalid authority;")
+	})
+
+	t.Run("success!", func(t *testing.T) {
+		msg := &types.MsgAddChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+		f.k.Params.Set(f.ctx, types.Params{Admin: validSigner.String()})
+		_, err := f.msgServer.AddChainConfig(f.ctx, msg)
+		require.NoError(t, err) // flag : need to add verify condition
+	})
+
+}
+
+func TestMsgServer_UpdateChainConfig(t *testing.T) {
+	f := SetupTest(t)
+	validSigner := f.addrs[0]
+
+	chainConfigTest := types.ChainConfig{
+		Chain:          "eip:11155111",
+		VmType:         types.VmType_EVM, // replace with appropriate VM_TYPE enum value
+		PublicRpcUrl:   "https://mainnet.infura.io/v3/YOUR_PROJECT_ID",
+		GatewayAddress: "0x1234567890abcdef1234567890abcdef12345678",
+		BlockConfirmation: &types.BlockConfirmation{
+			FastInbound: 3,
+			SlowInbound: 6,
+		},
+		GatewayMethods: []*types.GatewayMethods{},
+		Enabled:        true,
+	}
+
+	updatedChainConfigTest := types.ChainConfig{
+		Chain:          "eip:11155111",
+		VmType:         types.VmType_EVM, // replace with appropriate VM_TYPE enum value
+		PublicRpcUrl:   "https://mainnet.infura.io/v3/YOUR_PROJECT_ID",
+		GatewayAddress: "0x1234567890abcdef1234567890abcdef12345678",
+		BlockConfirmation: &types.BlockConfirmation{
+			FastInbound: 2,
+			SlowInbound: 8,
+		},
+		GatewayMethods: []*types.GatewayMethods{},
+		Enabled:        true,
+	}
+	t.Run("Failed to get params", func(t *testing.T) {
+		msg := &types.MsgUpdateChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+
+		_, err := f.msgServer.UpdateChainConfig(f.ctx, msg)
+		require.ErrorContains(t, err, "failed to get params")
+	})
+	t.Run("fail : Invalid authority", func(t *testing.T) {
+		msg := &types.MsgUpdateChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+		f.k.Params.Set(f.ctx, types.Params{})
+		_, err := f.msgServer.UpdateChainConfig(f.ctx, msg)
+		require.ErrorContains(t, err, "invalid authority;")
+	})
+
+	t.Run("fail : config does not exist to update", func(t *testing.T) {
+		msg := &types.MsgUpdateChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+		f.k.Params.Set(f.ctx, types.Params{Admin: validSigner.String()})
+		_, err := f.msgServer.UpdateChainConfig(f.ctx, msg)
+		require.ErrorContains(t, err, "chain config for eip:11155111 does not exist")
+	})
+
+	t.Run("success!", func(t *testing.T) {
+		addConfigMsg := &types.MsgAddChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &chainConfigTest,
+		}
+		f.k.Params.Set(f.ctx, types.Params{Admin: validSigner.String()})
+		_, err := f.msgServer.AddChainConfig(f.ctx, addConfigMsg)
+		require.NoError(t, err)
+
+		msg := &types.MsgUpdateChainConfig{
+			Signer:      validSigner.String(),
+			ChainConfig: &updatedChainConfigTest,
+		}
+		_, err = f.msgServer.UpdateChainConfig(f.ctx, msg)
+		require.NoError(t, err) // flag : need to add verify condition (cross-checking)
+	})
+}

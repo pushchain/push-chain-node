5c9d83928540eaeb7fc13694a963b7993f9c8a3c
Arya Lanjewar <102943033+AryaLanjewar3005@users.noreply.github.com>
2025-08-01 13:49:02 +0530

    modified integration testutils

--- x/ue/integration-test/testutils.go
+++ x/ue/integration-test/testutils.go
@@ -2,6 +2,8 @@ package integrationtest
 
 import (
 	"fmt"
+	"math/big"
+	"strings"
 	"testing"
 
 	log "cosmossdk.io/log"
@@ -15,9 +17,78 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	uetypes "github.com/rollchains/pchain/x/ue/types"
 	// "github.com/rollchains/pchain/utils"
 	// uetypes "github.com/rollchains/pchain/x/ue/types"
 )
+
+const FactoryABI = `[
+  {
+    "type": "function",
+    "name": "initialize",
+    "inputs": [
+      { "name": "initialOwner", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "registerNewChain",
+    "inputs": [
+      { "name": "_chainHash", "type": "bytes32", "internalType": "bytes32" },
+      { "name": "_vmHash", "type": "bytes32", "internalType": "bytes32" }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "deployUEA",
+    "inputs": [
+      {
+        "name": "_id",
+        "type": "tuple",
+        "internalType": "struct UniversalAccountId",
+        "components": [
+          { "name": "chainNamespace", "type": "string", "internalType": "string" },
+          { "name": "chainId", "type": "string", "internalType": "string" },
+          { "name": "owner", "type": "bytes", "internalType": "bytes" }
+        ]
+      }
+    ],
+    "outputs": [
+      { "name": "", "type": "address", "internalType": "address" }
+    ],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "computeUEA",
+    "inputs": [
+      {
+        "name": "_id",
+        "type": "tuple",
+        "internalType": "struct UniversalAccountId",
+        "components": [
+          { "name": "chainNamespace", "type": "string", "internalType": "string" },
+          { "name": "chainId", "type": "string", "internalType": "string" },
+          { "name": "owner", "type": "bytes", "internalType": "bytes" }
+        ]
+      }
+    ],
+    "outputs": [
+      { "name": "", "type": "address", "internalType": "address" }
+    ],
+    "stateMutability": "view"
+  }
+]`
 
 func SetupApp(t *testing.T) *app.ChainApp {
 	db := dbm.NewMemDB()
@@ -33,6 +104,10 @@ func SetAppWithValidators(t *testing.T) (*app.ChainApp, sdk.Context, sdk.Account
 	app := SetupApp(t)
 
 	ctx := app.BaseApp.NewContext(true)
+
+	evmParams := app.EVMKeeper.GetParams(ctx)
+	evmParams.ExtraEIPs = []string{"ethereum_3855"} // Enable PUSH0 opcode
+	app.EVMKeeper.SetParams(ctx, evmParams)
 
 	addr := sdk.AccAddress([]byte("0x778d3206374f8ac265728e18e3fe2ae6b93e4ce4"))
 	coins := sdk.NewCoins(sdk.NewInt64Coin("upc", 100000000)) // creates upc coins
@@ -66,11 +141,88 @@ func SetAppWithValidators(t *testing.T) (*app.ChainApp, sdk.Context, sdk.Account
 	acc_all := app.AccountKeeper.GetAllAccounts(ctx)
 	fmt.Println(acc_all)
 
-	contractAddr := common.HexToAddress("0x00000000000000000000000000000000000000ea")
-	evmAcc := app.EVMKeeper.GetAccountOrEmpty(ctx, contractAddr)
+	app.UeKeeper.InitGenesis(ctx, &uetypes.GenesisState{})
+
+	factoryAddr := common.HexToAddress("0x00000000000000000000000000000000000000ea")
+	evmAcc := app.EVMKeeper.GetAccountOrEmpty(ctx, factoryAddr)
 	code := app.EVMKeeper.GetCode(ctx, common.BytesToHash(evmAcc.CodeHash))
 
+	factoryABI, err := abi.JSON(strings.NewReader(FactoryABI))
+
+	// 4. Call factory.initialize(owner)
+	owner := common.BytesToAddress(addr.Bytes())
+	app.EVMKeeper.CallEVM(ctx, factoryABI, common.BytesToAddress(addr.Bytes()), factoryAddr, false, "initialize", owner)
+
+	require.NoError(t, err)
+
+	ownerAddr, _ := CallContractMethod(t, app, ctx, addr, factoryAddr, factoryABI, "getOwner")
+	fmt.Println("Stored owner:", common.BytesToAddress(ownerAddr))
+
+	evmHash := crypto.Keccak256Hash([]byte("EVM"))
+	evmSepoliaHash := crypto.Keccak256Hash([]byte("eip15511155111"))
+
+	app.EVMKeeper.CallEVM(
+		ctx,
+		factoryABI,
+		common.BytesToAddress(addr.Bytes()), // from
+		factoryAddr,                         // contract
+		false,                               // commit (stateful tx)
+		"registerNewChain",
+		[32]byte(evmSepoliaHash), // arg1: bytes32
+		[32]byte(evmHash),        // arg2: bytes32
+	)
+	require.NoError(t, err)
+
+	evmImplAddr := DeployContract(t, app, ctx, common.HexToAddress("0x0000000000000000000000000000000000000e01"), UEA_EVM_BYTECODE)
+
+	_, err = CallContractMethod(t, app, ctx, addr, factoryAddr, factoryABI, "registerUEA", evmSepoliaHash, evmHash, evmImplAddr)
+	require.NoError(t, err)
 	fmt.Println("Hmmmmmmmmm : ", code)
 
 	return app, ctx, acc
+}
+
+func DeployContract(
+	t *testing.T,
+	app *app.ChainApp,
+	ctx sdk.Context,
+	contractAddr common.Address,
+	bytecodeHex string,
+) common.Address {
+	bytecode, err := hexutil.Decode("0x" + bytecodeHex)
+	require.NoError(t, err)
+
+	codeHash := crypto.Keccak256Hash(bytecode)
+
+	evmAcc := app.EVMKeeper.GetAccountOrEmpty(ctx, contractAddr)
+	evmAcc.CodeHash = codeHash.Bytes()
+	app.EVMKeeper.SetAccount(ctx, contractAddr, evmAcc)
+	app.EVMKeeper.SetCode(ctx, codeHash.Bytes(), bytecode)
+
+	return contractAddr
+}
+
+func CallContractMethod(t *testing.T, app *app.ChainApp, ctx sdk.Context, from sdk.AccAddress, contract common.Address, contractABI abi.ABI, method string, args ...interface{}) ([]byte, error) {
+	input, err := contractABI.Pack(method, args...)
+	nonce := app.EVMKeeper.GetNonce(ctx, common.BytesToAddress(from.Bytes()))
+
+	msg := ethtypes.NewMessage(
+		common.BytesToAddress(from.Bytes()), // from
+		&contract,                           // to
+		nonce,                               // nonce
+		big.NewInt(0),                       // no value sent
+		5_000_000,                           // gas limit
+		big.NewInt(1_000_000_000),           // gas price
+		nil, nil,                            // no fee caps
+		input, // data (encoded method)
+		nil,   // access list
+		true,  // check nonce
+	)
+
+	res, err := app.EVMKeeper.ApplyMessage(ctx, msg, nil, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Ret, nil
 }

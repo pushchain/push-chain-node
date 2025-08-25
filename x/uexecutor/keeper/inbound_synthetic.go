ac7aafa6c09daf86532d96fdd4c39ec779952a3f
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-08-25 20:18:38 +0530

    feat: added keeper method for ExecuteInboundSynthetic

--- x/uexecutor/keeper/inbound_synthetic.go
+++ x/uexecutor/keeper/inbound_synthetic.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+<<<<<<< HEAD:x/uexecutor/keeper/inbound_synthetic.go
 
 	"github.com/rollchains/pchain/x/ue/types"
 )
@@ -30,4 +31,60 @@ func (k Keeper) IsPendingInboundSynthetic(ctx context.Context, inbound types.Inb
 func (k Keeper) RemovePendingInboundSynthetic(ctx context.Context, inbound types.InboundSynthetic) error {
 	key := types.GetInboundSyntheticKey(inbound)
 	return k.PendingInboundSynthetics.Remove(ctx, key)
+=======
+	"fmt"
+	"math/big"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rollchains/pchain/x/ue/types"
+)
+
+func (k Keeper) ExecuteInboundSynthetic(ctx context.Context, utx types.UniversalTx) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	tokenConfig, err := k.uregistryKeeper.GetTokenConfig(ctx, utx.InboundTx.SourceChain, utx.InboundTx.AssetAddr)
+	if err != nil {
+		return err
+	}
+
+	prc20Address := tokenConfig.NativeRepresentation.ContractAddress
+
+	// Convert inputs
+	prc20AddressHex := common.HexToAddress(prc20Address)
+	recipient := common.HexToAddress(utx.InboundTx.Recipient) // adjust field name
+	amount := new(big.Int)
+	amount, ok := amount.SetString(utx.InboundTx.Amount, 10) // assuming decimal string
+	if !ok {
+		return fmt.Errorf("invalid amount: %s", utx.InboundTx.Amount)
+	}
+
+	receipt, err := k.CallPRC20Deposit(sdkCtx, prc20AddressHex, recipient, amount)
+	if err != nil {
+		return err
+	}
+
+	_, ueModuleAddressStr := k.GetUeModuleAddress(ctx)
+
+	universalTxKey := types.GetInboundKey(*utx.InboundTx)
+	err = k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
+		pcTx := types.PCTx{
+			TxHash:      receipt.Hash,       // since tx didn’t go through
+			Sender:      ueModuleAddressStr, // or executor’s address if available
+			GasUsed:     receipt.GasUsed,
+			BlockHeight: uint64(sdkCtx.BlockHeight()),
+			Status:      "SUCCESS",
+			ErrorMsg:    "",
+		}
+
+		utx.PcTx = &pcTx
+		utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+>>>>>>> 51ad333 (feat: added keeper method for ExecuteInboundSynthetic):x/ue/keeper/inbound_synthetic.go
 }

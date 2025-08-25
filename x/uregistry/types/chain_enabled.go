2697f457c4f7066a118560651fb6d7f98b1f2389
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-08-25 13:13:04 +0530

    fix: fixed chain enabled changes

---
+++ x/uregistry/types/chain_enabled.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"encoding/json"
+)
+
+// Stringer method for Params.
+func (p ChainEnabled) String() string {
+	bz, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bz)
+}
+
+// Validate does the sanity check on the params.
+func (p ChainEnabled) ValidateBasic() error {
+	// Validate chain is non-empty and follows CAIP-2 format
+	// TODO: Implement Validation
+	return nil
+}

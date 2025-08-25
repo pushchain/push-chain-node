ab800ed0174e95d2270eb38c06799a10c992a71c
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-08-25 12:29:30 +0530

    refactor: added keeper methods for universalTx collection

---
+++ x/uexecutor/keeper/universal_tx.go
@@ -0,0 +1,64 @@
+package keeper
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+	"cosmossdk.io/collections"
+	"github.com/rollchains/pchain/x/ue/types"
+)
+
+// CreateUniversalTx stores a new UniversalTx
+func (k Keeper) CreateUniversalTx(ctx context.Context, key string, utx types.UniversalTx) error {
+	exists, err := k.UniversalTx.Has(ctx, key)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("universal tx with key %s already exists", key)
+	}
+
+	return k.UniversalTx.Set(ctx, key, utx)
+}
+
+// GetUniversalTx retrieves a UniversalTx by key, returns (value, found, error)
+func (k Keeper) GetUniversalTx(ctx context.Context, key string) (types.UniversalTx, bool, error) {
+	utx, err := k.UniversalTx.Get(ctx, key)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return types.UniversalTx{}, false, nil
+		}
+		return types.UniversalTx{}, false, err
+	}
+	return utx, true, nil
+}
+
+// HasUniversalTx checks if a UniversalTx exists
+func (k Keeper) HasUniversalTx(ctx context.Context, key string) (bool, error) {
+	return k.UniversalTx.Has(ctx, key)
+}
+
+// UpdateUniversalTxStatus sets a new status for the UniversalTx
+func (k Keeper) UpdateUniversalTxStatus(ctx context.Context, key string, newStatus types.UniversalTxStatus) error {
+	utx, err := k.UniversalTx.Get(ctx, key)
+	if err != nil {
+		return err
+	}
+
+	utx.UniversalStatus = newStatus
+	return k.UniversalTx.Set(ctx, key, utx)
+}
+
+// GetUniversalTxStatus retrieves the status of a UniversalTx
+func (k Keeper) GetUniversalTxStatus(ctx context.Context, key string) (types.UniversalTxStatus, bool, error) {
+	utx, found, err := k.GetUniversalTx(ctx, key)
+	if err != nil {
+		return types.UniversalTxStatus_UNIVERSAL_TX_STATUS_UNSPECIFIED, false, err
+	}
+	if !found {
+		return types.UniversalTxStatus_UNIVERSAL_TX_STATUS_UNSPECIFIED, false, nil
+	}
+	return utx.UniversalStatus, true, nil
+}

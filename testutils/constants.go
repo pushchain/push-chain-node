99d256e80c98c899b1b06fc58061a56436b695c8
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-09-10 18:39:01 +0530

    refactor: added setup of handler contract and prc20 contract

--- testutils/constants.go
+++ testutils/constants.go
@@ -8,14 +8,17 @@ const MintModule string = "mint"
 
 type Addresses struct {
 	// Contract addresses
-	FactoryAddr common.Address
-	UEProxyAddr common.Address
-	EVMImplAddr common.Address
+	FactoryAddr   common.Address
+	UEProxyAddr   common.Address
+	EVMImplAddr   common.Address
+	HandlerAddr   common.Address
+	PRC20USDCAddr common.Address
 
 	// Account addresses (hex format)
 	DefaultTestAddr string
 	CosmosTestAddr  string
 	TargetAddr      string
+	TargetAddr2     string
 }
 type TestConfig struct {
 	BaseCoinDenom  string
@@ -27,9 +30,12 @@ func GetDefaultAddresses() Addresses {
 		FactoryAddr:     common.HexToAddress("0x00000000000000000000000000000000000000ea"),
 		UEProxyAddr:     common.HexToAddress("0x0000000000000000000000000000000000000e09"),
 		EVMImplAddr:     common.HexToAddress("0x0000000000000000000000000000000000000e01"),
+		HandlerAddr:     common.HexToAddress("0x0000000000000000000000000000000000000e08"),
+		PRC20USDCAddr:   common.HexToAddress("0x0000000000000000000000000000000000000e06"),
 		DefaultTestAddr: "0x778d3206374f8ac265728e18e3fe2ae6b93e4ce4",
 		CosmosTestAddr:  "cosmos18pjnzwr9xdnx2vnpv5mxywfnv56xxef5cludl5",
 		TargetAddr:      "\x86i\xbe\xd1!\xfe\xfa=\x9c\xf2\x82\x12s\xf4\x89\xe7\x17̩]",
+		TargetAddr2:     "0x527F3692F5C53CfA83F7689885995606F93b6164",
 	}
 }
 

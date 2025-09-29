83dae2e9d073b6152160f6f3c9cb1bb7c22e10d3
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-09-29 13:01:49 +0530

    feat: updated inbound funds + payload tx execution as non-atomic

--- x/uexecutor/keeper/execute_inbound_funds_and_payload.go
+++ x/uexecutor/keeper/execute_inbound_funds_and_payload.go
@@ -6,13 +6,17 @@ import (
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	evmtypes "github.com/cosmos/evm/x/vm/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pushchain/push-chain-node/x/uexecutor/types"
 )
 
 func (k Keeper) ExecuteInboundFundsAndPayload(ctx context.Context, utx types.UniversalTx) error {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	_, ueModuleAddressStr := k.GetUeModuleAddress(ctx)
+	universalTxKey := types.GetInboundKey(*utx.InboundTx)
 
+	// Build universalAccountId
 	universalAccountId := types.UniversalAccountId{
 		ChainNamespace: strings.Split(utx.InboundTx.SourceChain, ":")[0],
 		ChainId:        strings.Split(utx.InboundTx.SourceChain, ":")[1],
@@ -20,73 +24,90 @@ func (k Keeper) ExecuteInboundFundsAndPayload(ctx context.Context, utx types.Uni
 	}
 
 	factoryAddress := common.HexToAddress(types.FACTORY_PROXY_ADDRESS_HEX)
-
 	ueModuleAccAddress, _ := k.GetUeModuleAddress(ctx)
 
-	// Call to check if uea is deployed
+	var execErr error
+	var receipt *evmtypes.MsgEthereumTxResponse
+
+	// --- Step 1: check factory for UEA
 	ueaAddr, isDeployed, err := k.CallFactoryToGetUEAAddressForOrigin(sdkCtx, ueModuleAccAddress, factoryAddress, &universalAccountId)
 	if err != nil {
-		return err
-	}
-
-	if !isDeployed {
-		return fmt.Errorf("UEA is not deployed")
-	}
-
-	receipt, err := k.depositPRC20(
-		sdkCtx,
-		utx.InboundTx.SourceChain,
-		utx.InboundTx.AssetAddr,
-		ueaAddr, // recipient is uea itself
-		utx.InboundTx.Amount,
-	)
-	if err != nil {
-		// TODO: update status to PendingRevert and add revert mechanism here
-		return err
-	}
-
-	ueModuleAddr, ueModuleAddressStr := k.GetUeModuleAddress(ctx)
-
-	universalTxKey := types.GetInboundKey(*utx.InboundTx)
-	err = k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
-		pcTx := types.PCTx{
-			TxHash:      receipt.Hash,       // TODO: handle revert params
-			Sender:      ueModuleAddressStr, // or executor’s address if available
-			GasUsed:     receipt.GasUsed,
-			BlockHeight: uint64(sdkCtx.BlockHeight()),
-			Status:      "SUCCESS", // TODO: handler revert params
-			ErrorMsg:    "",        // TODO: handler revert params
+		execErr = fmt.Errorf("factory lookup failed: %w", err)
+	} else if !isDeployed {
+		execErr = fmt.Errorf("UEA is not deployed")
+	} else {
+		// --- Step 2: deposit PRC20 into UEA
+		receipt, err = k.depositPRC20(
+			sdkCtx,
+			utx.InboundTx.SourceChain,
+			utx.InboundTx.AssetAddr,
+			ueaAddr,
+			utx.InboundTx.Amount,
+		)
+		if err != nil {
+			execErr = fmt.Errorf("depositPRC20 failed: %w", err)
 		}
+	}
 
-		utx.PcTx = append(utx.PcTx, &pcTx)
-		// utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
+	// --- record deposit attempt
+	depositPcTx := types.PCTx{
+		Sender:      ueModuleAddressStr,
+		BlockHeight: uint64(sdkCtx.BlockHeight()),
+		Status:      "FAILED",
+	}
+	if execErr != nil {
+		depositPcTx.ErrorMsg = execErr.Error()
+	} else {
+		depositPcTx.TxHash = receipt.Hash
+		depositPcTx.GasUsed = receipt.GasUsed
+		depositPcTx.Status = "SUCCESS"
+	}
+	updateErr := k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
+		utx.PcTx = append(utx.PcTx, &depositPcTx)
+		if execErr != nil {
+			utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_FAILED
+		}
 		return nil
 	})
-	if err != nil {
-		return err // TODO: revert mechanism
+	if updateErr != nil {
+		return updateErr
 	}
 
+	// If deposit failed, stop here (don’t attempt payload execution)
+	if execErr != nil {
+		return nil
+	}
+
+	// --- Step 3: execute payload
+	ueModuleAddr, _ := k.GetUeModuleAddress(ctx)
 	receipt, err = k.ExecutePayloadV2(ctx, ueModuleAddr, &universalAccountId, utx.InboundTx.UniversalPayload, utx.InboundTx.VerificationData)
-	if err != nil {
-		return err // TODO: revert mechanism
-	}
-	err = k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
-		pcTx := types.PCTx{
-			TxHash:      receipt.Hash, // TODO: handler revert params
-			Sender:      ueModuleAddressStr,
-			GasUsed:     receipt.GasUsed,
-			BlockHeight: uint64(sdkCtx.BlockHeight()),
-			Status:      "SUCCESS", // TODO: handler revert params
-			ErrorMsg:    "",        // TODO: handler revert params
-		}
 
-		utx.PcTx = append(utx.PcTx, &pcTx)
-		utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
+	payloadPcTx := types.PCTx{
+		Sender:      ueModuleAddressStr,
+		BlockHeight: uint64(sdkCtx.BlockHeight()),
+		Status:      "FAILED",
+	}
+	if err != nil {
+		payloadPcTx.ErrorMsg = err.Error()
+	} else {
+		payloadPcTx.TxHash = receipt.Hash
+		payloadPcTx.GasUsed = receipt.GasUsed
+		payloadPcTx.Status = "SUCCESS"
+	}
+
+	updateErr = k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
+		utx.PcTx = append(utx.PcTx, &payloadPcTx)
+		if err != nil {
+			utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_FAILED
+		} else {
+			utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
+		}
 		return nil
 	})
-	if err != nil {
-		return err
+	if updateErr != nil {
+		return updateErr
 	}
 
+	// never return execErr or err
 	return nil
 }

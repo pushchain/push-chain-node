30248a23a7c52835a3ed8ae7969a6fa75b379803
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-09-29 13:03:04 +0530

    feat: updated inbound gas tx execution as non-atomic

--- x/uexecutor/keeper/execute_inbound_gas_fund.go
+++ x/uexecutor/keeper/execute_inbound_gas_fund.go
@@ -2,86 +2,96 @@ package keeper
 
 import (
 	"context"
-	// "fmt"
-	// "math/big"
-	// "strings"
+	"fmt"
+	"math/big"
+	"strings"
 
-	// sdk "github.com/cosmos/cosmos-sdk/types"
-	// "github.com/ethereum/go-ethereum/common"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	evmtypes "github.com/cosmos/evm/x/vm/types"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/pushchain/push-chain-node/x/uexecutor/types"
-	// "github.com/pushchain/push-chain-node/utils"
-	// "cosmossdk.io/errors"
 )
 
 func (k Keeper) ExecuteInboundGasFund(ctx context.Context, inbound types.Inbound) error {
-	// sdkCtx := sdk.UnwrapSDKContext(ctx)
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	ueModuleAccAddress, ueModuleAddressStr := k.GetUeModuleAddress(ctx)
+	universalTxKey := types.GetInboundKey(inbound)
 
-	// tokenConfig, err := k.uregistryKeeper.GetTokenConfig(ctx, inbound.SourceChain, inbound.AssetAddr)
-	// if err != nil {
-	// 	return err
-	// }
+	// Default pcTx, will be filled along the way
+	pcTx := types.PCTx{
+		Sender:      ueModuleAddressStr,
+		BlockHeight: uint64(sdkCtx.BlockHeight()),
+		Status:      "FAILED", // assume failed until proven successful
+	}
 
-	// prc20Address := tokenConfig.NativeRepresentation.ContractAddress
+	var execErr error
+	var receipt *evmtypes.MsgEthereumTxResponse
 
-	// Convert inputs
-	// prc20AddressHex := common.HexToAddress(prc20Address)
-	// amount := new(big.Int)
-	// amount, ok := amount.SetString(inbound.Amount, 10) // assuming decimal string
-	// if !ok {
-	// 	return fmt.Errorf("invalid amount: %s", inbound.Amount)
-	// }
+	// --- step 1: get token config
+	tokenConfig, err := k.uregistryKeeper.GetTokenConfig(ctx, inbound.SourceChain, inbound.AssetAddr)
+	if err != nil {
+		execErr = fmt.Errorf("GetTokenConfig failed: %w", err)
+	} else {
+		// --- step 2: parse amount
+		amount := new(big.Int)
+		if amount, ok := amount.SetString(inbound.Amount, 10); !ok {
+			execErr = fmt.Errorf("invalid amount: %s", inbound.Amount)
+		} else {
+			// --- step 3: resolve / deploy UEA
+			prc20AddressHex := common.HexToAddress(tokenConfig.NativeRepresentation.ContractAddress)
+			universalAccountId := types.UniversalAccountId{
+				ChainNamespace: strings.Split(inbound.SourceChain, ":")[0],
+				ChainId:        strings.Split(inbound.SourceChain, ":")[1],
+				Owner:          inbound.Sender,
+			}
+			factoryAddress := common.HexToAddress(types.FACTORY_PROXY_ADDRESS_HEX)
 
-	// universalAccountId := types.UniversalAccountId{
-	// 	ChainNamespace: strings.Split(inbound.SourceChain, ":")[0],
-	// 	ChainId: strings.Split(inbound.SourceChain, ":")[1],
-	// 	Owner: inbound.Sender,
-	// }
+			ueaAddr, isDeployed, fErr := k.CallFactoryToGetUEAAddressForOrigin(sdkCtx, ueModuleAccAddress, factoryAddress, &universalAccountId)
+			if fErr != nil {
+				execErr = fmt.Errorf("CallFactory failed: %w", fErr)
+			} else {
+				if !isDeployed {
+					addr, dErr := k.DeployUEAV2(ctx, ueModuleAccAddress, &universalAccountId)
+					if dErr != nil {
+						execErr = fmt.Errorf("DeployUEA failed: %w", dErr)
+					} else {
+						ueaAddr = addr
+					}
+				}
 
-	// factoryAddress := common.HexToAddress(types.FACTORY_PROXY_ADDRESS_HEX)
+				if execErr == nil {
+					// --- step 4: deposit + swap
+					receipt, execErr = k.CallPRC20DepositAutoSwap(sdkCtx, prc20AddressHex, ueaAddr, amount)
+				}
+			}
+		}
+	}
 
-	// ueModuleAccAddress, _ := k.GetUeModuleAddress(ctx)
+	// --- Finalize pcTx
+	if execErr != nil {
+		pcTx.ErrorMsg = execErr.Error()
+	} else {
+		pcTx.TxHash = receipt.Hash
+		pcTx.GasUsed = receipt.GasUsed
+		pcTx.Status = "SUCCESS"
+		pcTx.ErrorMsg = ""
+	}
 
-	// Call to check if uea is deployed
-	// ueaAddr, isDeployed, err := k.CallFactoryToGetUEAAddressForOrigin(sdkCtx, ueModuleAccAddress, factoryAddress, &universalAccountId)
-	// if err != nil {
-	// 	return err
-	// }
+	// --- Update UniversalTx always
+	updateErr := k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
+		utx.PcTx = append(utx.PcTx, &pcTx)
+		if execErr != nil {
+			utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_FAILED
+		} else {
+			utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
+		}
+		return nil
+	})
+	if updateErr != nil {
+		// if state update fails, revert the tx
+		return updateErr
+	}
 
-	// if !isDeployed {
-	// 	addrStr, err := k.DeployUEAV2(ctx, ueModuleAccAddress, &universalAccountId)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	ueaAddr = common.HexToAddress(addrStr)
-	// }
-
-	// TODO: Call new deposit fn of handler that mints prc20 -> swap -> deposits to recipient
-	// receipt, err := k.CallPRC20Deposit(sdkCtx, prc20AddressHex, recipient, amount)
-	// if err != nil {
-	// 	// TODO: update status to PendingRevert and add revert mechanism here
-	// 	return err
-	// }
-
-	// _, ueModuleAddressStr := k.GetUeModuleAddress(ctx)
-
-	// universalTxKey := types.GetInboundKey(inbound)
-	// err = k.UpdateUniversalTx(ctx, universalTxKey, func(utx *types.UniversalTx) error {
-	// 	pcTx := types.PCTx{
-	// 		TxHash:      receipt.Hash,       // since tx didn’t go through
-	// 		Sender:      ueModuleAddressStr, // or executor’s address if available
-	// 		GasUsed:     receipt.GasUsed,
-	// 		BlockHeight: uint64(sdkCtx.BlockHeight()),
-	// 		Status:      "SUCCESS",
-	// 		ErrorMsg:    "",
-	// 	}
-
-	// 	utx.PcTx = &pcTx
-	// 	utx.UniversalStatus = types.UniversalTxStatus_PC_EXECUTED_SUCCESS
-	// return nil
-	// })
-	// if err != nil {
-	// 	return err
-	// }
-
+	// Never return execErr, only nil
 	return nil
 }

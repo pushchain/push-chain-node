24f8db1741e7daaf838ee2fc0e40571d8336c548
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-10-10 19:50:07 +0530

    feat: added upgrade handler for eth-hash-fix

---
+++ app/upgrades/eth-hash-fix/upgrades.go
@@ -0,0 +1,35 @@
+package inbound
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/pushchain/push-chain-node/app/upgrades"
+)
+
+const UpgradeName = "eth-hash-fix"
+
+// NewUpgrade constructs the upgrade definition
+func NewUpgrade() upgrades.Upgrade {
+	return upgrades.Upgrade{
+		UpgradeName:          UpgradeName,
+		CreateUpgradeHandler: CreateUpgradeHandler,
+	}
+}
+
+func CreateUpgradeHandler(
+	mm upgrades.ModuleManager,
+	configurator module.Configurator,
+	ak *upgrades.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info("🔧 Running upgrade:", "name", UpgradeName)
+
+		// Run module migrations
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}

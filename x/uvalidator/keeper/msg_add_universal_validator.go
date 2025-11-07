43fb157bc7cda93cd7928ec8a21a0607de198d37
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-07 13:19:07 +0530

    refactor: modified the msg_add_universal_validator as per uv structure change

--- x/uvalidator/keeper/msg_add_universal_validator.go
+++ x/uvalidator/keeper/msg_add_universal_validator.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/pushchain/push-chain-node/x/uvalidator/types"
 )
 
 // AddUniversalValidator registers a core validator as a universal validator.
 // It ensures the core validator exists, is bonded, and not already present.
 func (k Keeper) AddUniversalValidator(
 	ctx context.Context,
-	coreValidatorAddr string,
+	coreValidatorAddr, pubkey string,
+	networkInfo types.NetworkInfo,
 ) error {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 
@@ -39,9 +41,28 @@ func (k Keeper) AddUniversalValidator(
 		return fmt.Errorf("validator %s already registered", coreValidatorAddr)
 	}
 
+	initialStatus := types.UVStatus_UV_STATUS_PENDING_JOIN
+
+	uv := types.UniversalValidator{
+		IdentifyInfo: &types.IdentityInfo{
+			CoreValidatorAddress: coreValidatorAddr,
+			Pubkey:               pubkey,
+		},
+		LifecycleInfo: &types.LifecycleInfo{
+			CurrentStatus: initialStatus,
+			History: []*types.LifecycleEvent{
+				{
+					Status:      initialStatus,
+					BlockHeight: sdkCtx.BlockHeight(),
+				},
+			},
+		},
+		NetworkInfo: &networkInfo,
+	}
+
 	// Add universal validator to the set
-	if err := k.AddUniversalValidatorToSet(ctx, valAddr); err != nil {
-		return err
+	if err := k.UniversalValidatorSet.Set(ctx, valAddr, uv); err != nil {
+		return fmt.Errorf("failed to store universal validator: %w", err)
 	}
 
 	return nil

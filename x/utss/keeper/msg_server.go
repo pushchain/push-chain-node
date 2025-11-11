07744c825f011343b373328fb97a9889d865921d
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-11 12:17:59 +0530

    feat: added messages for initiating tss process and voting in proto

--- x/utss/keeper/msg_server.go
+++ x/utss/keeper/msg_server.go
@@ -20,10 +20,31 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{k: keeper}
 }
 
+// UpdateParams handles MsgUpdateParams for updating module parameters.
+// Only authorized governance account can execute this.
 func (ms msgServer) UpdateParams(ctx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
 	if ms.k.authority != msg.Authority {
 		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", ms.k.authority, msg.Authority)
 	}
 
-	return nil, ms.k.Params.Set(ctx, msg.Params)
+	err := ms.k.UpdateParams(ctx, msg.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgUpdateParamsResponse{}, nil
+}
+
+// InitiateTssKeyProcess implements types.MsgServer.
+func (ms msgServer) InitiateTssKeyProcess(ctx context.Context, msg *types.MsgInitiateTssKeyProcess) (*types.MsgInitiateTssKeyProcessResponse, error) {
+	// ctx := sdk.UnwrapSDKContext(goCtx)
+	panic("InitiateTssKeyProcess is unimplemented")
+	return &types.MsgInitiateTssKeyProcessResponse{}, nil
+}
+
+// VoteTssKeyProcess implements types.MsgServer.
+func (ms msgServer) VoteTssKeyProcess(ctx context.Context, msg *types.MsgVoteTssKeyProcess) (*types.MsgVoteTssKeyProcessResponse, error) {
+	// ctx := sdk.UnwrapSDKContext(goCtx)
+	panic("VoteTssKeyProcess is unimplemented")
+	return &types.MsgVoteTssKeyProcessResponse{}, nil
 }

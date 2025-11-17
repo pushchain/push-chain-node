d3d8a75013a411ff3e095ea471c1ea6a03ab9f99
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-17 12:57:14 +0530

    feat: added query server implementation

--- x/utss/keeper/query_server.go
+++ x/utss/keeper/query_server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"github.com/pushchain/push-chain-node/x/utss/types"
 )
@@ -18,6 +19,7 @@ func NewQuerier(keeper Keeper) Querier {
 	return Querier{Keeper: keeper}
 }
 
+// ---------------- Params ------------------
 func (k Querier) Params(c context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(c)
 
@@ -27,4 +29,106 @@ func (k Querier) Params(c context.Context, req *types.QueryParamsRequest) (*type
 	}
 
 	return &types.QueryParamsResponse{Params: &p}, nil
+}
+
+// ---------------- Current TSS Process ------------------
+func (k Querier) CurrentProcess(goCtx context.Context, req *types.QueryCurrentProcessRequest) (*types.QueryCurrentProcessResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	process, err := k.Keeper.CurrentTssProcess.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryCurrentProcessResponse{
+		Process: &process,
+	}, nil
+}
+
+// ---------------- Process By ID ------------------------
+func (k Querier) ProcessById(goCtx context.Context, req *types.QueryProcessByIdRequest) (*types.QueryProcessByIdResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	process, err := k.Keeper.ProcessHistory.Get(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryProcessByIdResponse{
+		Process: &process,
+	}, nil
+}
+
+// ---------------- All Processes (Paginated) -------------
+func (k Querier) AllProcesses(goCtx context.Context, req *types.QueryAllProcessesRequest) (*types.QueryAllProcessesResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	results, pageRes, err := query.CollectionPaginate(
+		ctx,
+		k.Keeper.ProcessHistory,
+		req.Pagination,
+		func(id uint64, process types.TssKeyProcess) (*types.TssKeyProcess, error) {
+			p := process
+			return &p, nil // return transformed object
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryAllProcessesResponse{
+		Processes:  results,
+		Pagination: pageRes,
+	}, nil
+}
+
+// ---------------- Current TSS Key -----------------------
+func (k Querier) CurrentKey(goCtx context.Context, req *types.QueryCurrentKeyRequest) (*types.QueryCurrentKeyResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	key, err := k.Keeper.CurrentTssKey.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryCurrentKeyResponse{
+		Key: &key,
+	}, nil
+}
+
+// ---------------- Key By ID -----------------------------
+func (k Querier) KeyById(goCtx context.Context, req *types.QueryKeyByIdRequest) (*types.QueryKeyByIdResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	key, err := k.Keeper.TssKeyHistory.Get(ctx, req.KeyId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryKeyByIdResponse{
+		Key: &key,
+	}, nil
+}
+
+// ---------------- All Keys (Paginated) -------------------
+func (k Querier) AllKeys(goCtx context.Context, req *types.QueryAllKeysRequest) (*types.QueryAllKeysResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	results, pageRes, err := query.CollectionPaginate(
+		ctx,
+		k.Keeper.TssKeyHistory,
+		req.Pagination,
+		func(id string, key types.TssKey) (*types.TssKey, error) {
+			kcopy := key
+			return &kcopy, nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryAllKeysResponse{
+		Keys:       results,
+		Pagination: pageRes,
+	}, nil
 }

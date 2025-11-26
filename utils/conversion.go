d032fcfc6b0178d04c0bf6f37a31e86be42003ed
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-26 14:58:49 +0530

    refactor: added a helper util to fetch the chain id dynamically

--- utils/conversion.go
+++ utils/conversion.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/hex"
+	"fmt"
 	"math/big"
 	"strings"
 )
@@ -13,4 +14,17 @@ func HexToBytes(s string) ([]byte, error) {
 func StringToBigInt(s string) *big.Int {
 	bi, _ := new(big.Int).SetString(s, 10)
 	return bi
+}
+
+// Returns evm chainId, e.g. push-chain-42101 -> 42101
+func ExtractEvmChainID(chainID string) (string, error) {
+	parts := strings.Split(chainID, "-")
+	last := parts[len(parts)-1]
+
+	// Ensure numeric
+	if _, ok := new(big.Int).SetString(last, 10); !ok {
+		return "", fmt.Errorf("invalid EVM chain id in tendermint chain-id: %s", chainID)
+	}
+
+	return last, nil
 }

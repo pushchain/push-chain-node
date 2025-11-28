e3c607223f5d7636220fcf428ac82c020c7cf30b
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-28 16:18:51 +0530

    refactor: added validation in networkInfo validateBasic

--- x/uvalidator/types/network_info.go
+++ x/uvalidator/types/network_info.go
@@ -7,12 +7,19 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// Validate does the sanity check on the params.
+// Validate does the sanity check on the network_info params.
 func (p NetworkInfo) ValidateBasic() error {
-	// Validate ip is non-empty
-	ip := strings.TrimSpace(p.Ip)
-	if ip == "" {
-		return errors.Wrap(sdkerrors.ErrInvalidRequest, "ip cannot be empty")
+	peerId := strings.TrimSpace(p.PeerId)
+	if peerId == "" {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "peerId cannot be empty")
+	}
+
+	if p.MultiAddrs == nil {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "multi_addrs cannot be nil")
+	}
+
+	if len(p.MultiAddrs) == 0 {
+		return errors.Wrap(sdkerrors.ErrInvalidRequest, "multi_addrs must contain at least one value")
 	}
 
 	return nil

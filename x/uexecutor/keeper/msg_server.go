3f8a24bcc843c8fb31284021a1ff55c0ecb60a09
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-11-28 11:19:37 +0530

    feat: added msg_vote_inbound msg server pre-check

--- x/uexecutor/keeper/msg_server.go
+++ x/uexecutor/keeper/msg_server.go
@@ -153,3 +153,38 @@ func (ms msgServer) VoteGasPrice(ctx context.Context, msg *types.MsgVoteGasPrice
 	}
 	return &types.MsgVoteGasPriceResponse{}, nil
 }
+
+// VoteOutbound implements types.MsgServer.
+func (ms msgServer) VoteOutbound(ctx context.Context, msg *types.MsgVoteOutbound) (*types.MsgVoteOutboundResponse, error) {
+	signerAccAddr, err := sdk.AccAddressFromBech32(msg.Signer)
+	if err != nil {
+		return nil, fmt.Errorf("invalid signer address: %w", err)
+	}
+
+	// Convert account to validator operator address
+	signerValAddr := sdk.ValAddress(signerAccAddr)
+
+	// Lookup the linked universal validator for this signer
+	isBonded, err := ms.k.uvalidatorKeeper.IsBondedUniversalValidator(ctx, msg.Signer)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to check bonded status for signer %s", msg.Signer)
+	}
+	if !isBonded {
+		return nil, fmt.Errorf("universal validator for signer %s is not bonded", msg.Signer)
+	}
+
+	isTombstoned, err := ms.k.uvalidatorKeeper.IsTombstonedUniversalValidator(ctx, msg.Signer)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to check tombstoned status for signer %s", msg.Signer)
+	}
+	if isTombstoned {
+		return nil, fmt.Errorf("universal validator for signer %s is tombstoned", msg.Signer)
+	}
+
+	err = ms.k.VoteOutbound(ctx, signerValAddr, msg.UtxId, msg.OutboundId, *msg.ObservedTx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgVoteOutboundResponse{}, nil
+}

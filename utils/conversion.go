c21b86b8d51776b155ee1f6d5c210f7839a53cce
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-12-15 12:47:22 +0530

    fix: fixed extraction of evm chainId in utils

--- utils/conversion.go
+++ utils/conversion.go
@@ -18,13 +18,23 @@ func StringToBigInt(s string) *big.Int {
 
 // Returns evm chainId, e.g. push-chain-42101 -> 42101
 func ExtractEvmChainID(chainID string) (string, error) {
-	parts := strings.Split(chainID, "-")
-	last := parts[len(parts)-1]
+	parts := strings.Split(chainID, "_")
+	if len(parts) != 2 {
+		return "", fmt.Errorf("invalid chain-id format: %s", chainID)
+	}
+
+	idPart := parts[1]
+	idParts := strings.Split(idPart, "-")
+	if len(idParts) < 1 {
+		return "", fmt.Errorf("invalid chain-id format: %s", chainID)
+	}
+
+	evmChainID := idParts[0]
 
 	// Ensure numeric
-	if _, ok := new(big.Int).SetString(last, 10); !ok {
+	if _, ok := new(big.Int).SetString(evmChainID, 10); !ok {
 		return "", fmt.Errorf("invalid EVM chain id in tendermint chain-id: %s", chainID)
 	}
 
-	return last, nil
+	return evmChainID, nil
 }

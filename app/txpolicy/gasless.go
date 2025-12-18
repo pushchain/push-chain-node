9ffbe2aa717c4ce638e86b189032f434fb63d587
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-12-18 15:34:34 +0530

    feat: created a txpolicy package for gasless and added vote tss key process in gasless messages

---
+++ app/txpolicy/gasless.go
@@ -0,0 +1,49 @@
+package txpolicy
+
+import (
+	"slices"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	uexecutortypes "github.com/pushchain/push-chain-node/x/uexecutor/types"
+	utsstypes "github.com/pushchain/push-chain-node/x/utss/types"
+)
+
+// IsGaslessTx checks if a transaction contains only allowed gasless message types
+// Returns true if all messages in the transaction are in the allowed gasless message types
+func IsGaslessTx(tx sdk.Tx) bool {
+	var (
+		// GaslessMsgTypes defines the message types that are allowed in gasless transactions
+		GaslessMsgTypes = []string{
+			sdk.MsgTypeURL(&uexecutortypes.MsgMigrateUEA{}),
+			sdk.MsgTypeURL(&uexecutortypes.MsgExecutePayload{}),
+			sdk.MsgTypeURL(&uexecutortypes.MsgDeployUEA{}),
+			sdk.MsgTypeURL(&uexecutortypes.MsgMintPC{}),
+			sdk.MsgTypeURL(&uexecutortypes.MsgVoteInbound{}),
+			sdk.MsgTypeURL(&uexecutortypes.MsgVoteGasPrice{}),
+			sdk.MsgTypeURL(&utsstypes.MsgVoteTssKeyProcess{}),
+		}
+	)
+
+	msgs := tx.GetMsgs()
+	if len(msgs) == 0 {
+		return false
+	}
+
+	for _, msg := range msgs {
+		switch m := msg.(type) {
+		case *authz.MsgExec:
+			// Only gasless if ALL inner messages are allowed
+			for _, innerMsg := range m.Msgs {
+				if !slices.Contains(GaslessMsgTypes, innerMsg.TypeUrl) {
+					return false
+				}
+			}
+		default:
+			if !slices.Contains(GaslessMsgTypes, sdk.MsgTypeURL(msg)) {
+				return false
+			}
+		}
+	}
+	return true
+}

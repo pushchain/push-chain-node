d8220f963e350f4344fea0370e599dbd38148967
aman035 <guptaaman200115@gmail.com>
2026-01-15 14:51:11 +0530

    constant changes

--- universalClient/constant/constant.go
+++ universalClient/constant/constant.go
@@ -4,18 +4,21 @@ import "os"
 
 // <NodeDir>/                    (e.g., /home/universal/.puniversal)
 // └── config/
-//
 //	└── pushuv_config.json
+// └── databases/
+//	└── eip155_1.db
+//	└── eip155_97.db
+
 const (
 	NodeDir = ".puniversal"
 
 	ConfigSubdir   = "config"
 	ConfigFileName = "pushuv_config.json"
+
+	DatabasesSubdir = "databases"
 )
 
-var (
-	DefaultNodeHome = os.ExpandEnv("$HOME/") + NodeDir
-)
+var DefaultNodeHome = os.ExpandEnv("$HOME/") + NodeDir
 
 // RequiredMsgGrants contains all the required message type URLs
 // that must be granted via AuthZ for the Universal Validator to function.
@@ -23,6 +26,6 @@ var (
 var RequiredMsgGrants = []string{
 	"/uexecutor.v1.MsgVoteInbound",
 	"/uexecutor.v1.MsgVoteGasPrice",
-	"/uexecutor.v1.MsgVoteOutbound",
+	// "/uexecutor.v1.MsgVoteOutbound",
 	"/utss.v1.MsgVoteTssKeyProcess",
 }

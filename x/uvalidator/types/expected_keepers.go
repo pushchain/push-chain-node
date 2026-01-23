9c2a72628619471f2c5d86c40b53bb498a88aec6
Nilesh Gupta <guptanilesh2312@gmail.com>
2026-01-23 18:51:44 +0530

    feat: added bank, account, distribution module types in expected_keepers.go of uvalidator module

--- x/uvalidator/types/expected_keepers.go
+++ x/uvalidator/types/expected_keepers.go
@@ -3,6 +3,8 @@ package types
 import (
 	"context"
 
+	"cosmossdk.io/core/address"
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -11,6 +13,7 @@ import (
 type StakingKeeper interface {
 	GetValidator(ctx context.Context, addr sdk.ValAddress) (validator stakingtypes.Validator, err error)
 	GetAllValidators(ctx context.Context) (validators []stakingtypes.Validator, err error)
+	ValidatorByConsAddr(context.Context, sdk.ConsAddress) (stakingtypes.ValidatorI, error)
 }
 
 // SlashingKeeper defines the expected interface for the slashing module.
@@ -22,4 +25,30 @@ type SlashingKeeper interface {
 type UtssKeeper interface {
 	GetCurrentTssParticipants(ctx context.Context) ([]string, error)
 	HasOngoingTss(ctx context.Context) (bool, error)
+}
+
+type AccountKeeper interface {
+	AddressCodec() address.Codec
+	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	GetModuleAddress(name string) sdk.AccAddress
+	GetModuleAccount(ctx context.Context, name string) sdk.ModuleAccountI
+}
+
+// BankKeeper defines the expected interface needed to retrieve account balances.
+type BankKeeper interface {
+	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+
+	SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+
+	BlockedAddr(addr sdk.AccAddress) bool
+}
+
+type DistributionKeeper interface {
+	FundCommunityPool(ctx context.Context, amount sdk.Coins, sender sdk.AccAddress) error
+	GetCommunityTax(ctx context.Context) (math.LegacyDec, error)
+	AllocateTokensToValidator(ctx context.Context, val stakingtypes.ValidatorI, tokens sdk.DecCoins) error
 }

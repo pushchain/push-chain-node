dce51024283730f614d620cba1736c066a641956
Nilesh Gupta <guptanilesh2312@gmail.com>
2026-03-12 16:11:19 +0530

    feat: added upgrade handler

---
+++ app/upgrades/chain-meta/upgrade.go
@@ -0,0 +1,41 @@
+package chainmeta
+
+import (
+	"context"
+
+	storetypes "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/pushchain/push-chain-node/app/upgrades"
+)
+
+const UpgradeName = "chain-meta"
+
+// NewUpgrade constructs the upgrade definition
+func NewUpgrade() upgrades.Upgrade {
+	return upgrades.Upgrade{
+		UpgradeName:          UpgradeName,
+		CreateUpgradeHandler: CreateUpgradeHandler,
+		StoreUpgrades: storetypes.StoreUpgrades{
+			Added:   []string{},
+			Deleted: []string{},
+		},
+	}
+}
+
+func CreateUpgradeHandler(
+	mm upgrades.ModuleManager,
+	configurator module.Configurator,
+	ak *upgrades.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info("🔧 Running upgrade:", "name", UpgradeName)
+
+		// RunMigrations triggers uexecutor v4→v5 which calls MigrateGasPricesToChainMeta.
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
